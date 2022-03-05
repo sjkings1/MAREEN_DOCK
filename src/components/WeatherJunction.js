71e12af9505b12edb2bea8515f0474965b5a2bab
sjkings1 <samsonjarald111@gmail.com>
2022-03-05 12:39:06 +0530

    update

--- src/components/WeatherJunction.js
+++ src/components/WeatherJunction.js
@@ -14,6 +14,7 @@ import { getForecastWeather, getForecastWeatherSuccess, getForecastWeatherFailur
 
 import './WeatherPage_Current.css';
 import "./WeatherPage_Forecast.css";
+import "./WeatherJunction.css"
 import { displayWeekDay, displayDayDate, displayTime, KelvinToCelsius } from './DataConverter';
 
 import { Card, CardContent, CardMedia, Typography, CardActionArea, Divider, Paper, Grid, Avatar, TextField, Button } from '@mui/material';
@@ -25,6 +26,9 @@ import ShowChartIcon from '@mui/icons-material/ShowChart';
 
 import { Field, Form, Formik, ErrorMessage } from 'formik';
 
+import PlacesAutocomplete from 'react-places-autocomplete';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+
 
 function WeatherJunction(props) {
 
@@ -32,6 +36,16 @@ function WeatherJunction(props) {
     const [lat, setLat] = useState([]);
     const [lon, setLon] = useState([]);
     const dispatch = useDispatch();
+
+    const [address, setAddress] = useState("");
+    const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
+
+    const handleSelect = async value => {
+        const results = await geocodeByAddress(value);
+        const latlng = await getLatLng(results[0]);
+        setAddress(value);
+        setCoordinates(latlng);
+    }
 
     // WEATHERPAGE_CURRENT
     const { currentWeather, loadingC, hasErrorsC } = useSelector(currentWeatherSelector);
@@ -41,13 +55,22 @@ function WeatherJunction(props) {
 
     useEffect(() => {
 
-        navigator.geolocation.getCurrentPosition(function (position) {
-            setLat(position.coords.latitude);
-            setLon(position.coords.longitude);
-        });
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.watchPosition(showPosition);
+            } else {
+                // x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+        function showPosition() {
 
-        dispatch(fetchCurrentWeather(lat, lon))
-        dispatch(fetchForecastWeather(lat, lon))
+            dispatch(fetchCurrentWeather(lat, lon))
+            dispatch(fetchForecastWeather(lat, lon))
+
+            // dispatch(fetchCurrentWeather(position.coords.latitude, position.coords.longitude))
+            // dispatch(fetchForecastWeather(position.coords.latitude, position.coords.longitude))
+        }
+        getLocation();
 
     }, [])
 
@@ -56,7 +79,7 @@ function WeatherJunction(props) {
         return (async dispatch => {
             dispatch(getCurrentWeather())
             try {
-                const currentSkyData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9df3f65223f5d0da919ec90525134833`)
+                const currentSkyData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4cd569ffb3ecc3bffe9c0587ff02109f`)
                 debugger
                 dispatch(getCurrentWeatherSuccess(currentSkyData.data))
             } catch (error) {
@@ -64,13 +87,13 @@ function WeatherJunction(props) {
             }
         })
     }
-
+debugger
     function fetchForecastWeather(lat, lon) {
 
         return (async dispatch => {
             dispatch(getForecastWeather())
             try {
-                const currentSpaceData = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}.04&appid=9df3f65223f5d0da919ec90525134833`)
+                const currentSpaceData = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=4cd569ffb3ecc3bffe9c0587ff02109f`)
                 debugger
                 dispatch(getForecastWeatherSuccess(currentSpaceData.data))
             } catch (error) {
@@ -99,37 +122,71 @@ function WeatherJunction(props) {
 
     let forecastData = renderForecastWeather();
 
-
-
     return (
         <div className='weatherJunction'>
 
+            <div className='getWeatherClick'>
+                <Grid className="">
+                    <Paper elevation={10}>
+                        <Formik>
+                            {() => (
+                                <Form>
+                                    <Field as={TextField} id="outlined-required" value={lat} onChange={data => setLat(data.target.value)} label="Latitude" type="number" name="latitude" placeholder="latitude" fullWidth />
+                                    <Field as={TextField} id="outlined-required" value={lon} onChange={data => setLon(data.target.value)} label="Longitude" type="number" name="longitude" placeholder="longitude" fullWidth />
+                                    <Button variant="contained" onClick={
+                                        () => {
+                                            dispatch(fetchCurrentWeather(lat, lon));
+                                            dispatch(fetchForecastWeather(lat, lon));
+                                        }
+                                    } fullWidth> GET WEATHER </Button>
+                                </Form>
+                            )}
+                        </Formik>
+                    </Paper>
+                </Grid>
 
-            <Grid className="">
-                <Paper elevation={10}>
-                    <Formik>
-                        {() => (
-                            <Form>
-                                <Field as={TextField} id="outlined-required" value={lat} onChange={e => setLat(e.target.value)} label="Latitude" type="number" name="latitude" placeholder="latitude" fullWidth />
-                                <Field as={TextField} id="outlined-required" value={lon} onChange={e => setLon(e.target.value)} label="Longitude" type="number" name="longitude" placeholder="longitude" fullWidth />
-                                <Button variant="contained" onClick={() => dispatch(fetchCurrentWeather(lat, lon))} fullWidth> GET WEATHER </Button>
-                            </Form>
-                        )}
-                    </Formik>
-                </Paper>
-            </Grid>
+                <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelect}>
+
+                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                    <div>
+
+                        <p> LATITUDE: {coordinates.lat} </p>
+                        <p> LONGITUDE: {coordinates.lng} </p>
 
 
+                        <input {...getInputProps({ placeholder: "address type it" })} />
+
+                        <div>
+
+                            {/* {loading ? <div>...loading</div> : null} */}
+
+                            {suggestions.map(e => {
+
+                                const style = {
+                                    backgroundColor: e.active ? "#0CACFF" : "#C5C5C5"
+                                };
 
 
+                                return (
+                                    <div {...getSuggestionItemProps(e, { style })}>
+                                        {e.description}
+                                    </div>
+                                );
+                            })}
+
+                        </div>
+
+                    </div>
+                )}
+
+            </PlacesAutocomplete>
 
 
+            </div>
 
             <div className='weatherPage_current'>
 
                 <section>
-
-
 
                     <h1> CURRENT WEATHER PAGE </h1>
                     <div className='content'> {currentData}  </div>
@@ -187,8 +244,6 @@ function WeatherJunction(props) {
             </div>
 
 
-
-
             <div className='weatherPage_forecast'>
 
                 <section>
@@ -202,7 +257,7 @@ function WeatherJunction(props) {
 
                             <CardContent>
 
-                                <Typography gutterBottom variant="h4" component="div"> <h1 className='country_name'> {forecastWeather.timezone} </h1> </Typography>
+                                <Typography gutterBottom variant="h4" component="div"> <h1 className='country_name'> {currentWeather?.sys?.country} / {currentWeather.name}  </h1> </Typography>
 
                                 <Grid align='left'> <h3 className='next_7_days'> Next 7 Days </h3> </Grid>
 
@@ -243,11 +298,6 @@ function WeatherJunction(props) {
                 </section>
 
             </div>
-
-
-
-
-
 
 
         </div>
